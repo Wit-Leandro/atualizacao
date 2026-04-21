eafe3e64528ea5eb8d6eb9f5c439286f09971d43
Wit-Leandro <158459855+Wit-Leandro@users.noreply.github.com>
2026-04-21 18:16:08 -0300

    Update alert messages in script.js

--- script.js
+++ script.js
@@ -91,8 +91,9 @@ window.addEventListener('load', function () {
   var local = 'Não informado'
   dic.push(nome, endereco, ncasa, fone, local)
   loadCounter();
-  //alert("Atendimento normal apenas na loja hoje\n das 14hs as 23hs")
-  alert("\u{1F4CC}\nCONCLUA TODAS AS ETAPAS \nATÉ A ABA DE PAGAMENTOS \nPARA FINALIZAR SEU PEDIDO")
+  alert("Atendimento normal apenas na loja hoje\n das 14hs as 23hs")
+  alert("Hoje não teremos entregas delivery \n Apenas retirada na loja")
+  //alert("\u{1F4CC}\nCONCLUA TODAS AS ETAPAS \nATÉ A ABA DE PAGAMENTOS \nPARA FINALIZAR SEU PEDIDO")
 
 })
 
